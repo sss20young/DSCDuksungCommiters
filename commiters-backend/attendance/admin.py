8d1787cb730f6e765b90e5421a61ce60ef470825
Seoyoung Kim <ksyt98@gmail.com>
2020-10-13 19:41:10 +0900

    change typo

--- commiters-backend/attendance/admin.py
+++ commiters-backend/attendance/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import User, Repository
 
 # Register your models here.
-admin.sie.register(User)
-admin.sie.register(Repository)
+admin.site.register(User)
+admin.site.register(Repository)
